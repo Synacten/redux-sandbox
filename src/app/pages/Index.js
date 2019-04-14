72ed07d443145f01d1b7c43974d3ba2d674aa01e
Synacten <blackhorns9999@gmail.com>
2019-04-14 19:18:15 +0300

    add static data

--- src/app/pages/Index.js
+++ src/app/pages/Index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Index = () => (
-  <div>
-    index
-  </div>
-);
+export class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: 'Jonh Dou',
+          email: 'jdou@gmail.com',
+          phone: '111-1111-111',
+        },
+        {
+          id: 2,
+          name: 'Berry Smith',
+          email: 'bsmith@gmail.com',
+          phone: '333-3333-333',
+        },
+        {
+          id: 1,
+          name: 'Kate Gross',
+          email: 'kgross@gmail.com',
+          phone: '777-7777-777',
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        {contacts.map(contact => (
+          <div key={contact.id}>
+            <h2>{contact.name}</h2>
+            <p>{contact.email}</p>
+            <p>{contact.phone}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
