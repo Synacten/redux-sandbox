53bcd8d6a6ef70027ed627abd8474e15656fc393
Andrey <blachorns9999@gmail.com>
2019-04-18 16:14:55 +0300

    render search results on page

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -5,25 +5,39 @@ import styled from 'styled-components';
 
 import { getModal, getSearchResults } from '../actions/contactsActions';
 
-const Modal = props => (
-  <ModalC>
-        modal window (MODAL)
-    <div role="presentation" onClick={() => props.getModal()}>close modal in modal component</div>
-    <div className="searchInput">
-      <input type="text" name="search" onChange={e => props.getSearchResults(e.target.value)} />
-    </div>
-  </ModalC>
-);
+const Modal = (props) => {
+  const { resultSearch } = props;
+  return (
+    <ModalC>
+          modal window (MODAL)
+      <div role="presentation" onClick={() => props.getModal()}>close modal in modal component</div>
+      <div className="searchInput">
+        <input type="text" name="search" onChange={e => props.getSearchResults(e.target.value)} />
+      </div>
+      <div>
+        {resultSearch.map(n => (
+          <div key={n.id}>
+            <h1>{n.name}</h1>
+            <h4>{n.email}</h4>
+            <h5>{n.company.catchPhrase}</h5>
+          </div>
+        ))}
+      </div>
+    </ModalC>
+  );
+};
 
 
 Modal.propTypes = {
   getModal: PropTypes.func.isRequired,
   getSearchResults: PropTypes.func.isRequired,
+  resultSearch: PropTypes
+    .arrayOf(PropTypes.oneOfType([PropTypes.array, PropTypes.object])).isRequired,
 };
 
 const mapStateToProps = state => ({
   openModal: state.monitor.isOpen,
-  inputTEST: state.monitor.testInput,
+  resultSearch: state.monitor.searchResult,
 });
 
 
